c058d1ee7792131f7747f12e1b7eca9dafe40997
Alan Diaz <adiazny@gmail.com>
2018-07-10 10:27:43 -0400

    Updated upload script to include code for lambda dunction

--- upload_portfolio_lambda.py
+++ upload_portfolio_lambda.py
@@ -3,22 +3,26 @@ import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    
+    s3 = boto3.resource('s3')
+    
+    portfolio_bucket = s3.Bucket('portfolio.alandiaz.com')
+    
+    build_bucket = s3.Bucket('portfoliobuild.alandiaz.com')
+    
+    portfolio_zip = io.BytesIO()
+    
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    return 'Hello from Lambda'
 
-portfolio_bucket = s3.Bucket('portfolio.alandiaz.com')
-
-build_bucket = s3.Bucket('portfoliobuild.alandiaz.com')
-
-portfolio_zip = io.BytesIO()
-
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
 
 #Iterate through the bucket to list the objects.
