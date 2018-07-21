a41f92aad8b94dd4be92c435a06c2dc7c203cb99
Alan Diaz <adiazny@gmail.com>
2018-07-21 16:19:48 -0400

    Updated React Components

--- js/example-work-modal.js
+++ js/example-work-modal.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-class ExampleWorkModal extends react.Component {
+class ExampleWorkModal extends React.Component {
 
   render() {
     let example = this.props.example;
+    let modalClass = this.props.open ? 'modal--open' : 'modal--closed';
 
     return (
-      <div className="background--skyBlue modal--closed">
-        <span className="color--cloud modal__closeButton">
+      <div className={"background--skyBlue " + modalClass}>
+        <span className="color--cloud modal__closeButton"
+          onClick={this.props.closeModal}>
           <i className="fa fa-window-close-o"></i>
         </span>
         <img alt={ example.image.desc }
--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const myWork = [
   {
     'title': "A Serverless Portforlio",
     'href' : "https://example.com",
-    'desc' : "A Serverless Portfolio using AWS technology",
+    'desc' : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image' : {
       'desc' : "example screenshot of a project inolving code",
       'src' : "images/example1.png",
@@ -16,7 +16,7 @@ const myWork = [
   {
     'title': "Work Example 2 - Coming Soon",
     'href' : "https://example.com",
-    'desc' : "A Serverless Portfolio using AWS technology",
+    'desc' : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image' : {
       'desc' : "example screenshot of a project inolving code",
       'src' : "images/example2.png",
